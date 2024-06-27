9d50516dad03b418f90774ead80ad05451259b72
jusohn Eddie Sohn <jusohn@student.42seoul.kr>
2024-06-27 13:39:36 +0900

    feat: created mock subtitle data #1

---
+++ pages/content-ui/src/constants/data.ts
@@ -0,0 +1,100 @@
+import { Subtitles } from '@src/hooks/useSubtitles';
+
+export const data: Subtitles = {
+  wireMagic: 'pb3',
+  pens: [{}],
+  wsWinStyles: [{}],
+  wpWinPositions: [{}],
+  events: [
+    {
+      tStartMs: 467,
+      dDurationMs: 2000,
+      segs: [
+        {
+          utf8: 'I have built the most\ndeadly obstacle course in the world,',
+        },
+      ],
+    },
+    {
+      tStartMs: 3000,
+      dDurationMs: 1000,
+      segs: [
+        {
+          utf8: 'and my friend Mack here',
+        },
+      ],
+    },
+    {
+      tStartMs: 5238,
+      dDurationMs: 4037,
+      segs: [
+        {
+          utf8: 'is attempting to complete it\nwithout falling 200 feet to the ground.',
+        },
+      ],
+    },
+    {
+      tStartMs: 9275,
+      dDurationMs: 1335,
+      segs: [
+        {
+          utf8: "Whenever you're ready, Mack.",
+        },
+      ],
+    },
+    {
+      tStartMs: 10610,
+      dDurationMs: 1735,
+      segs: [
+        {
+          utf8: "First jump, he's about to do it.",
+        },
+      ],
+    },
+    {
+      tStartMs: 14280,
+      dDurationMs: 1402,
+      segs: [
+        {
+          utf8: "Oh my God, I'm so high up in the air.",
+        },
+      ],
+    },
+    {
+      tStartMs: 15682,
+      dDurationMs: 1768,
+      segs: [
+        {
+          utf8: 'Look at how much\nthat platform is shaking.',
+        },
+      ],
+    },
+    {
+      tStartMs: 17450,
+      dDurationMs: 1969,
+      segs: [
+        {
+          utf8: 'We are currently hundreds of feet\nin the air.',
+        },
+      ],
+    },
+    {
+      tStartMs: 19419,
+      dDurationMs: 767,
+      segs: [
+        {
+          utf8: 'Look at that.',
+        },
+      ],
+    },
+    {
+      tStartMs: 20186,
+      dDurationMs: 3671,
+      segs: [
+        {
+          utf8: 'And this is just\nthe first of many deadly, painful,',
+        },
+      ],
+    },
+  ],
+};
