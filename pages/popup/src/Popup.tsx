e82bc91d9b25bf548031ad11638eccee6ff5878c
jusohn Eddie Sohn <jusohn@student.42seoul.kr>
2024-06-27 14:33:28 +0900

    fix: updated Popup to use updated storage, updated <img> <label> tag #2

--- pages/popup/src/Popup.tsx
+++ pages/popup/src/Popup.tsx
@@ -1,29 +1,22 @@
-import React, { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { useStorageSuspense, withErrorBoundary, withSuspense } from '@chrome-extension-boilerplate/shared';
-import { exampleThemeStorage } from '@chrome-extension-boilerplate/storage';
-import { storage, saveSettings, loadSettings } from '@chrome-extension-boilerplate/storage/lib/settingStorage';
+import { subtitleSettingStorage } from '@chrome-extension-boilerplate/storage';
+
+type View = 'home' | 'options';
 
 const Popup = () => {
-  const theme = useStorageSuspense(exampleThemeStorage);
-  const [activeView, setActiveView] = useState('home');
-  const [color, setColor] = useState('#FFFFFF');
-  const [background, setBackground] = useState('#191919');
-  const [font, setFont] = useState('Arial');
-  const [spacing, setSpacing] = useState(0);
-  const [stroke, setStroke] = useState(0);
-  const [size, setSize] = useState(12);
+  const subtitleSetting = useStorageSuspense(subtitleSettingStorage);
 
-  const resetSettings = () => {
-    setColor('#FFFFFF');
-    setBackground('#191919');
-    setFont('Arial');
-    setSpacing(0);
-    setStroke(0);
-    setSize(12);
-  };
+  const [activeView, setActiveView] = useState<View>('home');
+  const [color, setColor] = useState<string>(subtitleSetting.color);
+  const [background, setBackground] = useState<string>(subtitleSetting.background);
+  const [font, setFont] = useState<string>(subtitleSetting.font);
+  const [spacing, setSpacing] = useState<number>(subtitleSetting.spacing);
+  const [stroke, setStroke] = useState<number>(subtitleSetting.stroke);
+  const [size, setSize] = useState<number>(subtitleSetting.size);
 
-  const saveSettings = async () => {
-    await storage.set({
+  const saveSettings = useCallback(() => {
+    subtitleSettingStorage.saveSettings({
       color,
       background,
       font,
@@ -31,14 +24,12 @@ const Popup = () => {
       stroke,
       size,
     });
-    // const savedSettings = await storage.get();
-    // console.log('Saved Settings:', savedSettings);
-  };
+  }, [color, background, font, spacing, stroke, size]);
 
   return (
     <div className="w-72 bg-gray-800 flex flex-col items-center">
       <div className="flex w-full items-center justify-start h-14 gap-4 px-2">
-        <img className="w-10 h-10 rounded" src={chrome.runtime.getURL('popup/logo.png')} />
+        <img className="w-10 h-10 rounded" alt="smartCC logo" src={chrome.runtime.getURL('popup/logo.png')} />
         <button className="text-sm text-white" id="home-btn" onClick={() => setActiveView('home')}>
           Home
         </button>
@@ -66,16 +57,25 @@ const Popup = () => {
           <p className="w-full text-center mb-4">Customize your subtitles to match your preferences.</p>
           <div className="flex flex-wrap w-full mb-2">
             <div className="flex flex-col w-1/2 px-2">
-              <label className="text-gray-600">Font</label>
-              <select value={font} onChange={e => setFont(e.target.value)} className="p-2 border rounded w-full h-8">
+              <label htmlFor="font-type" className="text-gray-600">
+                Font
+              </label>
+              <select
+                id="font-type"
+                value={font}
+                onChange={e => setFont(e.target.value)}
+                className="p-2 border rounded w-full h-8">
                 <option>Arial</option>
                 <option>Verdana</option>
                 <option>Helvetica</option>
               </select>
             </div>
             <div className="flex flex-col w-1/2 px-2">
-              <label className="text-gray-600">Size</label>
+              <label htmlFor="font-size" className="text-gray-600">
+                Size
+              </label>
               <input
+                id="font-size"
                 type="number"
                 className="p-2 border rounded w-full h-8"
                 value={size}
@@ -83,9 +83,12 @@ const Popup = () => {
               />
             </div>
             <div className="flex flex-col w-1/2 px-2">
-              <label className="text-gray-600">Color</label>
+              <label htmlFor="font-color" className="text-gray-600">
+                Color
+              </label>
               <div className="relative border rounded h-8 w-full bg-white flex items-center cursor-pointer">
                 <input
+                  id="font-color"
                   type="color"
                   className="absolute inset-0 w-full h-full opacity-0 cursor-pointer"
                   value={color}
@@ -99,9 +102,12 @@ const Popup = () => {
               </div>
             </div>
             <div className="flex flex-col w-1/2 px-2">
-              <label className="text-gray-600">Background</label>
+              <label htmlFor="caption-bg-color" className="text-gray-600">
+                Background
+              </label>
               <div className="relative border rounded h-8 w-full bg-white flex items-center cursor-pointer">
                 <input
+                  id="caption-bg-color"
                   type="color"
                   className="absolute inset-0 w-full h-full opacity-0 cursor-pointer"
                   value={background}
@@ -115,8 +121,11 @@ const Popup = () => {
               </div>
             </div>
             <div className="flex flex-col w-1/2 px-2">
-              <label className="text-gray-600">Stroke</label>
+              <label htmlFor="caption-stroke" className="text-gray-600">
+                Stroke
+              </label>
               <input
+                id="caption-stroke"
                 type="number"
                 className="p-2 border rounded w-full h-8"
                 value={stroke}
@@ -124,8 +133,11 @@ const Popup = () => {
               />
             </div>
             <div className="flex flex-col w-1/2 px-2">
-              <label className="text-gray-600">Spacing</label>
+              <label htmlFor="caption-spacing" className="text-gray-600">
+                Spacing
+              </label>
               <input
+                id="caption-spacing"
                 type="number"
                 className="p-2 border rounded w-full h-8"
                 value={spacing}
@@ -134,7 +146,9 @@ const Popup = () => {
             </div>
           </div>
           <div className="flex w-full justify-around mt-4 gap-1">
-            <button className="bg-slate-300 text-black py-1.5 px-3 rounded w-24" onClick={resetSettings}>
+            <button
+              className="bg-slate-300 text-black py-1.5 px-3 rounded w-24"
+              onClick={subtitleSettingStorage.resetSettings}>
               Reset
             </button>
             <button className="bg-teal-500 text-white py-1.5 px-3 rounded w-24" onClick={saveSettings}>
